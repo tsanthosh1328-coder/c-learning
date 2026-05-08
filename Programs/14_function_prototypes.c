7015caa88694bc647a59e9cbe40e8858564485a4
Santhosh <tsanthosh1328@gmail.com>
2026-05-08 09:52:00 +0530

    function prototypes

--- Programs/14_function_prototypes.c
+++ Programs/14_function_prototypes.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 
+void printHeader(char name[], int grade);
+void printSubject(char subject[], int score);
+
+int main() {
+
+    char name[30];
+    int grade;
+    char subject1[15], subject2[15], subject3[15];
+    int score1, score2, score3;
+
+    printf("Enter student name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0'; 
+
+    printf("Enter grade: ");
+    scanf("%d", &grade);
+    
+    printf("Enter subject 1 : ");
+    scanf("%s", &subject1);
+
+    printf("Enter score 1: ");
+    scanf("%d", &score1);
+
+    printf("Enter subject 2 : ");
+    scanf("%s", &subject2);
+
+    printf("Enter score 2: ");
+    scanf("%d", &score2);
+
+    printf("Enter subject 3 : ");
+    scanf("%s", &subject3);
+
+    printf("Enter score 3: ");
+    scanf("%d", &score3);
+
+    printHeader(name, grade);
+    printSubject(subject1, score1);
+    printSubject(subject2, score2);
+    printSubject(subject3, score3);
+
+    return 0;
+}
+
 void printHeader(char name[], int grade) {
+    printf("\n");
     printf("====== Report Card ======\n");
     printf("Student Name: %s\n", name);
     printf("Grade: %d\n", grade);
@@ -20,44 +64,4 @@ void printSubject(char subject[], int score) {
 
     printf("%-10s  : %d  --  %s\n", subject, score, status);
 
-}
-
-int main() {
-
-    char name[30];
-    int grade;
-    char subject1[15], subject2[15], subject3[15];
-    int score1, score2, score3;
-
-    printf("Enter student name: ");
-    fgets(name, sizeof(name), stdin);
-    name[strlen(name) - 1] = '\0'; 
-
-    printf("Enter grade: ");
-    scanf("%d", &grade);
-
-    printf("Enter subject 1 : ");
-    scanf("%s", &subject1);
-    
-    printf("Enter score: ");
-    scanf("%d", &score1);
-
-    printf("Enter subject 2 : ");
-    scanf("%s", &subject2);
-    
-    printf("Enter score: ");
-    scanf("%d", &score2);
-
-    printf("Enter subject 3 : ");
-    scanf("%s", &subject3);
-    
-    printf("Enter score: ");
-    scanf("%d", &score3);
-    
-    printHeader(name, grade);
-    printSubject(subject1, score1);
-    printSubject(subject2, score2);
-    printSubject(subject3, score3);
-
-    return 0;
 }
